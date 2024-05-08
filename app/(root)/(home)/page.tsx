6cd258f48c5a2e0981f106e8c70d62618962a175
Ayush <ayushsinghlts123@gmail.com>
2024-05-08 12:37:53 +0530

    HomePage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,32 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react'
 
-type Props = {}
+const Home = () => {
 
-const Home = (props: Props) => {
+const now = new Date();
+
+const time = now.toLocaleTimeString('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'Asia/Kolkata' });
+const date = (new Intl.DateTimeFormat('en-IN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(now));
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+              Upcoming meeeting at 10:00 AM
+          </h2>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-extrabold lg:text-7xl'>
+              {time}
+            </h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+              {date}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
     </section>
   )
 }
